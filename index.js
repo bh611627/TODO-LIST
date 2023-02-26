18b82fd96715a4c5d3a8e0b2e2db000d69d0de73
Bilal Hassan <49266029+bh611627@users.noreply.github.com>
2023-02-26 12:13:13 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 function AddTask() {
-    const taskInput = document.getElementById("Task-Input");
-    const taskList = document.getElementById("task-unorder-list");
-    
-    // Get current date and time
-    const dateTime = new Date().toLocaleString();
+  const taskInput = document.getElementById("Task-Input");
+  const taskList = document.getElementById("task-unorder-list");
   
-    // Create new list item with task and time
-    const li = document.createElement("li");
-    li.innerText = taskInput.value;
-    
-    // Create span element for the timestamp
-    const timestamp = document.createElement("span");
-    timestamp.textContent = dateTime;
-    
-    // Append timestamp to list item
-    li.appendChild(timestamp);
-    
-    // Create delete button
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "delete";
-    deleteButton.innerText = "x";
-    
-    // Add delete button to list item
-    li.appendChild(deleteButton);
-    
-    // Add list item to task list
-    taskList.appendChild(li);
-    
-    // Clear task input
-    taskInput.value = "";
-    
-    // Attach click event listener to delete button
-    deleteButton.addEventListener("click", function() {
-      taskList.removeChild(li);
-    });
+  // Blank Input or Empty Input Error
+  if (!taskInput.value.trim()) {
+    alert("Please Enter The Task");
+    return;
   }
+
+  // Get Time
+  const DateTime = new Date().toLocaleString();
+
+  // Add New Item List In Li 
+  const li = document.createElement('li');
+  li.textContent = taskInput.value;
+
+  // Create a Time span 
+  const timespan = document.createElement('span');
+  timespan.id = 'date-time'
+  timespan.textContent = DateTime;
+
+  // Create A Re-Edit Button
+  const Edit = document.createElement('a');
+  Edit.classList.add('edit');
+  Edit.textContent = 'EDIT';
+
+  // Create A Delete Button
+  const Delete = document.createElement('a');
+  Delete.classList.add('delete');
+  Delete.textContent = 'DELETE';
+
+  // Append The Re-Edit, Delete, Time In List
+  li.appendChild(timespan);
+  li.appendChild(Edit);
+  li.appendChild(Delete); 
+
+  // Add list item to task list
+  taskList.appendChild(li);
+
+  // Clear The Input Value 
+  taskInput.value = '';
+
+  // Delete Event Listeners Function
+  Delete.addEventListener("click", function() {
+    taskList.removeChild(li);
+  });
+
+  // Re-Edit Event Listener Function
+  Edit.addEventListener('click', function() {
+    const currentTask = li.firstChild.textContent;
+    const newTask = prompt('Enter the new task:', currentTask);
+    
+    if (newTask && newTask.trim() !== '') {
+      li.firstChild.textContent = newTask.trim();
+    }
+  });
+  
+}
